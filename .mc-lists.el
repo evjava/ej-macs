841227f519e32a49510d9222a0cb5f8b14f6444e
evjava <evjava@yandex.ru>
2023-03-04 13:13:30 +0300

    master: updated .gitignore, .mc-lists.el

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,18 +3,71 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        LaTeX-insert-left-brace
+        TeX-insert-backslash
+        backward-sexp
+        backward-up-list
+        c-electric-colon
+        c-electric-delete-forward
+        c-electric-lt-gt
+        c-electric-paren
+        c-electric-pound
+        c-electric-semi&comma
         c-electric-slash
+        c-indent-line-or-region
+        comint-delchar-or-maybe-eof
         dabbrev-expand
+        dired-clean-directory
+        dired-toggle-read-only
+        ej/add-line-item
+        ej/copy-all
+        ej/eval-replace
+        ej/insert-latex-section
+        ej/kill-line
+        ej/shell-1-or-else
+        end-of-buffer
+        eros-eval-last-sexp
         forward-sexp
+        haskell-indentation-common-electric-command
+        helm-M-x
+        helm-show-kill-ring
+        indent-for-tab-command
         kill-region
+        kmacro-start-macro-or-insert-counter
         mc/vertical-align-with-space
+        org-attach
+        org-beginning-of-line
         org-ctrl-c-ctrl-c
+        org-cycle
+        org-delete-char
         org-end-of-line
         org-force-self-insert
+        org-forward-sentence
         org-kill-line
+        org-open-line
         org-self-insert-command
+        org-shifttab
+        org-yank
+        pg-protected-undo
+        revert-buffer
+        slime-space
+        split-window-horizontally
+        test
+        transpose-words
+        upcase-region
+        wdired--self-insert
+        wdired-capitalize-word
+        wdired-downcase-word
+        wdired-exit
+        wdired-next-line
+        wdired-previous-line
         ))
 
 (setq mc/cmds-to-run-once
       '(
+        dired-next-line
+        dired-previous-line
+        kmacro-end-and-call-macro
+        yaml-electric-dash-and-dot
+        yank-rectangle
         ))
